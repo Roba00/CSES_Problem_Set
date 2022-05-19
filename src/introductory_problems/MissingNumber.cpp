95e0a6651871918ad76f5daebc0c0e4d99d3e638
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2022-05-18 20:22:02 -0500

    MissingNumber problem finished

--- src/introductory_problems/MissingNumber.cpp
+++ src/introductory_problems/MissingNumber.cpp
@@ -1,15 +1,33 @@
 #include "MissingNumber.h"
 #include <iostream>
-#include <vector>
 
 int MissingNumberMain()
 {
+    // Reads size
     unsigned long n;
     std::cin >> n;
 
-    std::vector<int> array();
-    for (int i = 0; i < n - 1; i++) {
-
+    // Creates boolean array to represent number indexes
+    bool numbers[n + 1];
+    for (unsigned int i = 1; i < n + 1; i++) {
+        numbers[i] = false;
     }
+    numbers[0] = true;
+
+    // Iteratively reads numbers in second line,
+    // and calls true if each number is read
+    for (unsigned int i = 0; i < n - 1; i++) {
+        int tempNumber;
+        std::cin >> tempNumber;
+        numbers[tempNumber] = true;
+    }
+
+    // Prints out missing number
+    for (unsigned int i = 1; i < n + 1; i++) {
+        if (!numbers[i]) {
+            std::cout << i;
+        }
+    }
+
     return 0;
 }
